deba71093e9efb042592ba83c5c78042587edc3c
LongTran <longhzredux@gmail.com>
2026-03-18 19:43:54 +0700

    feat(auth): implement auth service

---
+++ backend/src/main/java/com/example/backend/service/AuthService.java
@@ -0,0 +1,49 @@
+package com.example.backend.service;
+
+import com.example.backend.dto.AuthResponse;
+import com.example.backend.dto.LoginRequest;
+import com.example.backend.dto.RegisterRequest;
+import com.example.backend.entity.User;
+import com.example.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .id(UUID.randomUUID().toString())
+                .fullName(request.fullName())
+                .email(request.email())
+                .passwordHash(passwordEncoder.encode(request.password()))
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return new AuthResponse(jwtToken);
+    }
+
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.email(),
+                        request.password()
+                )
+        );
+        var user = userRepository.findByEmail(request.email())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return new AuthResponse(jwtToken);
+    }
+}
