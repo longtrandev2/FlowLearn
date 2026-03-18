16463781175fdb2964901b80b33ffb6fb9ba55aa
LongTran <longhzredux@gmail.com>
2026-03-18 20:53:17 +0700

    feat(admin): support user search specifications

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -2,11 +2,12 @@ package com.example.backend.repository;
 
 import com.example.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String>, JpaSpecificationExecutor<User> {
     Optional<User> findByEmail(String email);
 }
